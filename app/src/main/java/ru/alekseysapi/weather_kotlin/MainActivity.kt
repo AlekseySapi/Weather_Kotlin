4f08fb83484f993fdbde3a06f29eb669351eeec7
AlekseySapi <altmind92@gmail.com>
2022-07-01 07:24:28 +0300

    Рассмотрел применение ViewBinding

--- app/src/main/java/ru/alekseysapi/weather_kotlin/MainActivity.kt
+++ app/src/main/java/ru/alekseysapi/weather_kotlin/MainActivity.kt
@@ -2,10 +2,16 @@ package ru.alekseysapi.weather_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.alekseysapi.weather_kotlin.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+internal class MainActivity: AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+        setContentView(binding.activityRoot)
+        binding.btn.text = "Change text"
     }
 }
